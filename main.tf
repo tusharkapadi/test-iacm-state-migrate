bc058a69da3116f9e52fc22f61de83b97cf8498a
Tushar Kapadi <tushar.kapadi@gmail.com>
2025-06-26 17:38:25 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "my_first_ec2_instance" {
   ami = "ami-0418306302097dbff" 
-  instance_type = "t2.micro" # Got to https://aws.amazon.com/ec2/instance-types/t2/ for a full T2 instance type list.
+  instance_type = "t2.small" # Got to https://aws.amazon.com/ec2/instance-types/t2/ for a full T2 instance type list.
   
   tags = {
     Name = "my_first_ec2_instance"
